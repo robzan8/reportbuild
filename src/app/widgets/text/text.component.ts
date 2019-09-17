353bd6be103019992f46ebe6180d7d634fb9e6e0
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-09-17 11:17:31 +0200

    chchange for text widget

--- src/app/widgets/text/text.component.ts
+++ src/app/widgets/text/text.component.ts
@@ -1,5 +1,9 @@
 import { AjfTextWidget } from '@ajf/core/reports';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewEncapsulation, Optional } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component,
+  ViewEncapsulation, Optional, OnDestroy } from '@angular/core';
+
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { ReportBuilderComponent } from '../../report-builder/report-builder.component';
 import { WidgetComponent } from '../widget/widget.component';
@@ -11,16 +15,34 @@ import { WidgetComponent } from '../widget/widget.component';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TextComponent extends WidgetComponent {
+export class TextComponent extends WidgetComponent implements OnDestroy {
+
+  keyUp = new Subject<Event>();
+  private sub: Subscription;
+
   get text(): AjfTextWidget {
     return this.widget as AjfTextWidget;
   }
 
   constructor(@Optional() builder: ReportBuilderComponent, cdr: ChangeDetectorRef) {
     super(builder, cdr);
+    this.sub = this.keyUp.pipe(debounceTime(150)).subscribe(event => {
+      this.onTextChange(event);
+      this.cdr.markForCheck();
+    });
   }
 
   onTextChange(event: Event) {
     (this.widget as AjfTextWidget).htmlText = (event.target as HTMLTextAreaElement).value;
   }
+
+  onKeyup(e: Event) {
+    e.stopPropagation();
+    this.keyUp.next(e);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
